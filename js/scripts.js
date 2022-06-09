881052b6a84796cef7b5b1ae1953766ea2e60403
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-08 20:56:52 -0700

    add mathematical functions in pop up

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+// business logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("The sum is " + add(3,5) + ".");
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
+function devide(number1, number2) {
+  return number1 / number2;
+}
+
+// user interface logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
+
+window.alert(subtract(number1, number2));
+
+window.alert(multiply(number1, number2));
+
+window.alert(devide(number1, number2));
